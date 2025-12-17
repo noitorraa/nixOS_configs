94a6643e965d01c4e1a79e1cb0d7c4918bf51910
noitorraa <ivankonuk00@gmail.com>
2025-12-17 12:27:55 +0700

    add flake.nix and home.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "My NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:rycee/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+    nixosConfigurations.my-system = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useUserPackages = true;
+          home-manager.users.qz7renna = ./home-manager/home.nix;
+        }
+      ];
+    };
+  };
+}
+
